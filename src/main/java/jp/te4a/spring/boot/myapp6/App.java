c1f9e75487c5d91692ee4817cad5b89e0e7d9104
岩崎 拓海 <180046@jc-21.jp>
2021-06-03 15:09:22 +0900

    init

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/App.java
@@ -0,0 +1,10 @@
+package jp.te4a.spring.boot.myapp6;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
